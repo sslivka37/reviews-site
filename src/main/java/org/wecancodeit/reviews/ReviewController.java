f06bdab734104a9ddda2f06f803a30b6567c5841
Your Name <you@example.com>
2019-02-11 11:44:19 -0500

    Sarah second commit, still can't get images to work, tried adding a resource path in the application properties

--- src/main/java/org/wecancodeit/reviews/ReviewController.java
+++ src/main/java/org/wecancodeit/reviews/ReviewController.java
@@ -6,7 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+
+
 
 @Controller
 public class ReviewController {
@@ -29,10 +30,6 @@ public class ReviewController {
 	}
 	
 	
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry
-          .addResourceHandler("/resources/**")
-          .addResourceLocations("/resources/"); 
-    }
+  
 
 }
--- src/main/java/org/wecancodeit/reviews/ReviewsApplication.java
+++ src/main/java/org/wecancodeit/reviews/ReviewsApplication.java
@@ -3,6 +3,7 @@ package org.wecancodeit.reviews;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ReviewsApplication {
 
