b6181d9fa64982a69118254a8a915bf3a4f91ae0
Your Name <you@example.com>
2019-02-12 10:14:31 -0500

    Sarah third commit to reviews, images working just need formatting

--- src/main/java/org/wecancodeit/reviews/ReviewsApplication.java
+++ src/main/java/org/wecancodeit/reviews/ReviewsApplication.java
@@ -4,12 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
+
+
+
 @SpringBootApplication
-public class ReviewsApplication {
+public class ReviewsApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReviewsApplication.class, args);
 	}
+	
 
 }
 
